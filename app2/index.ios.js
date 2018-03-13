9263389cb982080076f84edf57cf8398e958e200
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-03-12 23:34:30 -0300

    Brincando com padding e margin

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -4,16 +4,17 @@ import { Text, AppRegistry } from 'react-native';
 //Formatacoes
 const Estilos = {
   estiloTexto: {
-    fontSize: 50
+    fontSize: 30,
+    backgroundColor: '#08509B',  
+    padding: 30
   }
 };
 
 //Criar component
 const App = () => {
   return (
-    <Text style={ Estilos.estiloTexto }>Frases do dia</Text>
+    <Text style={ Estilos.estiloTexto } >Frases do dia</Text>
   );
 };
-
 
 AppRegistry.registerComponent('app2', () => App);
