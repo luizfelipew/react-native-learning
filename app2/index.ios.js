755db72e180ddd24dadb134603fc6ef879da26c7
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-03-14 21:38:07 -0300

    Formatacao de textos

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -4,12 +4,19 @@ import { Text, AppRegistry } from 'react-native';
 //Formatacoes
 const Estilos = {
   estiloTexto: {
-    fontSize: 20,
+    fontSize: 40,
     backgroundColor: '#08509B',  
     paddingTop: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.4
+    color: '#fff',
+    //fontStyle: 'italic'
+    //fontWeight: '300' //100 e 900
+    //textAlign: 'left'
+
+    textDecorationLine: 'underline'
+
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.4
   }
 };
 
